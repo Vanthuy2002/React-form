d670c1d7d91eb97be495b4b43197db0a0d2537ae
Thuy Nguyen <vanthuy101102@gmail.com>
2023-05-24 00:37:48 +0700

    set up base formik

--- src/components/form/RegisterFormik.jsx
+++ src/components/form/RegisterFormik.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RegisterFormik = () => {
-  return <div></div>;
+const RegisterFormik = (props) => {
+  return (
+    <div>
+      <h1>This is form by formik</h1>
+    </div>
+  );
 };
+
+RegisterFormik.propTypes = {};
 
 export default RegisterFormik;
